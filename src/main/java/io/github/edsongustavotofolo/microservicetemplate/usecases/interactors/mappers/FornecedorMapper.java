4c8b0dc5b85d1151b85127a8abfec7a98f450582
Edson Tofolo <edsontofolo@gmail.com>
2022-12-29 11:50:29 -0300

    ajustado mapper do fornecedor

--- src/main/java/io/github/edsongustavotofolo/microservicetemplate/usecases/interactors/mappers/FornecedorMapper.java
+++ src/main/java/io/github/edsongustavotofolo/microservicetemplate/usecases/interactors/mappers/FornecedorMapper.java
@@ -1,10 +1,16 @@
 package io.github.edsongustavotofolo.microservicetemplate.usecases.interactors.mappers;
 
+import io.github.edsongustavotofolo.microservicetemplate.domain.entities.Endereco;
 import io.github.edsongustavotofolo.microservicetemplate.domain.entities.Fornecedor;
+import io.github.edsongustavotofolo.microservicetemplate.domain.entities.impl.CidadeImpl;
+import io.github.edsongustavotofolo.microservicetemplate.domain.entities.impl.EnderecoImpl;
 import io.github.edsongustavotofolo.microservicetemplate.domain.entities.impl.FornecedorImpl;
+import io.github.edsongustavotofolo.microservicetemplate.domain.entities.valueobjects.Cep;
 import io.github.edsongustavotofolo.microservicetemplate.domain.entities.valueobjects.Cnpj;
 import io.github.edsongustavotofolo.microservicetemplate.usecases.ports.input.dtos.CreateFornecedor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = {ContatoMapper.class})
@@ -16,7 +22,21 @@ public interface FornecedorMapper {
         return toDomain(createFornecedor);
     }
 
+    @Named("mapEndereco")
+    default Endereco mapEndereco(final CreateFornecedor createFornecedor) {
+        return new EnderecoImpl(
+                createFornecedor.getLogradouro(),
+                createFornecedor.getNumero(),
+                createFornecedor.getBairro(),
+                createFornecedor.getComplemento(),
+                createFornecedor.getPontoDeReferencia(),
+                new Cep(createFornecedor.getCep()),
+                new CidadeImpl(createFornecedor.getCidade())
+        );
+    }
+
     Cnpj map(final String numero);
 
+    @Mapping(target = "endereco", source = ".", qualifiedByName = "mapEndereco")
     FornecedorImpl toDomain(final CreateFornecedor createFornecedor);
 }
