59d8d933951bc7f6bd18516acb64f7ed87612552
Edson Tofolo <edsontofolo@gmail.com>
2022-12-29 11:15:03 -0300

    ajustado teste unitario do createfornecedor

--- src/test/java/io/github/edsongustavotofolo/microservicetemplate/usecases/interactors/CreateFornecedorInteractorUnitTest.java
+++ src/test/java/io/github/edsongustavotofolo/microservicetemplate/usecases/interactors/CreateFornecedorInteractorUnitTest.java
@@ -1,11 +1,9 @@
 package io.github.edsongustavotofolo.microservicetemplate.usecases.interactors;
 
 import io.github.edsongustavotofolo.microservicetemplate.domain.entities.Fornecedor;
-import io.github.edsongustavotofolo.microservicetemplate.interfaceadapters.presenters.exceptions.FornecedorCnpjInvalidException;
 import io.github.edsongustavotofolo.microservicetemplate.usecases.ports.output.CreateFornecedorOutputPort;
 import io.github.edsongustavotofolo.microservicetemplate.usecases.ports.output.exceptions.BusinessRuleException;
 import io.github.edsongustavotofolo.microservicetemplate.usecases.providers.FornecedorProvider;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -19,8 +17,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,44 +38,23 @@ class CreateFornecedorInteractorUnitTest {
     private ArgumentCaptor<Integer> idArgumentCaptor;
 
     @Test
-    void deveLancarExcecaoAoCriarFornecedorComCnpjJaExistenteNaBase() throws BusinessRuleException {
-        final var expectedMessage = "Já existe Fornecedor com o CNPJ informado!";
-
-        final var ex = new FornecedorCnpjInvalidException();
-
-        doThrow(ex).when(this.presenter).fornecedorAlreadyExists();
-
+    void naoDeveCriarFornecedorQuandoCnpjInformadoJaExiste() throws BusinessRuleException {
         when(this.fornecedorProvider.existsFornecedorWithCnpj(anyString())).thenReturn(true);
 
-        // execucao
-        try {
-            this.interactor.execute(createFornecedor().build());
-            Assertions.fail("Deveria lançar exceção de fornecedor já existente com cnpj informado");
-        } catch (final Exception actualException) {
-            assertEquals(FornecedorCnpjInvalidException.class, actualException.getClass());
-            assertEquals(expectedMessage, actualException.getMessage());
-        }
+        this.interactor.execute(createFornecedor().build());
+
+        verify(this.presenter).fornecedorAlreadyExists();
+
+        verifyNoMoreInteractions(this.fornecedorProvider);
     }
 
     @Test
-    void deveLancarExcecaoAoCriarFornecedorComCnpjInvalido() throws BusinessRuleException {
-        // cenario
-        final var expectedMessage = "CNPJ inválido!";
+    void naoDeveCriarFornecedorQuandoCnpjInformadoForInvalido() throws BusinessRuleException {
+        this.interactor.execute(createFornecedor().cnpj("99999999999999").build());
 
-        final var expectedException = new FornecedorCnpjInvalidException();
-        doThrow(expectedException).when(this.presenter).cnpjIsInvalid();
+        verify(this.presenter).cnpjIsInvalid();
 
-        final var cnpjInvalido = "99999999999999";
-        final var requestModel = createFornecedor().cnpj(cnpjInvalido).build();
-
-        // execucao
-        try {
-            this.interactor.execute(requestModel);
-            Assertions.fail("Deveria lançar exceção de CNPJ inválido");
-        } catch (final Exception actualException) {
-            assertEquals(expectedException.getClass(), actualException.getClass());
-            assertEquals(expectedException.getMessage(), actualException.getMessage());
-        }
+        verifyNoInteractions(this.fornecedorProvider);
     }
 
     @Test
