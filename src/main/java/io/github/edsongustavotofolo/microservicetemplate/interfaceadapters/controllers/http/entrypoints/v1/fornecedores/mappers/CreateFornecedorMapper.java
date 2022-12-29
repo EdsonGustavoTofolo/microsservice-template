9c6e4dc3ac3e4eda05390959f90a61987f9730b8
Edson Tofolo <edsontofolo@gmail.com>
2022-12-29 14:59:52 -0300

    ajustado mapper do request para o dto do inputport

--- src/main/java/io/github/edsongustavotofolo/microservicetemplate/interfaceadapters/controllers/http/entrypoints/v1/fornecedores/mappers/CreateFornecedorMapper.java
+++ src/main/java/io/github/edsongustavotofolo/microservicetemplate/interfaceadapters/controllers/http/entrypoints/v1/fornecedores/mappers/CreateFornecedorMapper.java
@@ -3,6 +3,7 @@ package io.github.edsongustavotofolo.microservicetemplate.interfaceadapters.cont
 import io.github.edsongustavotofolo.microservicetemplate.interfaceadapters.controllers.http.entrypoints.v1.fornecedores.dtos.CreateFornecedorRequest;
 import io.github.edsongustavotofolo.microservicetemplate.usecases.ports.input.dtos.CreateFornecedor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +11,12 @@ public interface CreateFornecedorMapper {
 
     CreateFornecedorMapper INSTANCE = Mappers.getMapper(CreateFornecedorMapper.class);
 
+    @Mapping(target = "logradouro", source = "endereco.logradouro")
+    @Mapping(target = "numero", source = "endereco.numero")
+    @Mapping(target = "bairro", source = "endereco.bairro")
+    @Mapping(target = "complemento", source = "endereco.complemento")
+    @Mapping(target = "pontoDeReferencia", source = "endereco.pontoDeReferencia")
+    @Mapping(target = "cep", source = "endereco.cep")
+    @Mapping(target = "cidade", source = "endereco.cidade")
     CreateFornecedor toModel(final CreateFornecedorRequest requestModel);
 }
