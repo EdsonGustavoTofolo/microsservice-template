28d95a0f52807873e1f0480acc49fca91f3227f8
Edson Tofolo <edsontofolo@gmail.com>
2023-01-01 11:10:48 -0300

    ajustada consulta por id do fornecedor

--- src/main/java/io/github/edsongustavotofolo/microservicetemplate/interfaceadapters/gateways/database/repository/FornecedorJpaRepository.java
+++ src/main/java/io/github/edsongustavotofolo/microservicetemplate/interfaceadapters/gateways/database/repository/FornecedorJpaRepository.java
@@ -1,16 +1,24 @@
 package io.github.edsongustavotofolo.microservicetemplate.interfaceadapters.gateways.database.repository;
 
 import io.github.edsongustavotofolo.microservicetemplate.interfaceadapters.gateways.database.model.FornecedorEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 @Transactional(readOnly = true)
 public interface FornecedorJpaRepository
         extends RevisionRepository<FornecedorEntity, Integer, Integer>,
-                JpaRepository<FornecedorEntity, Integer>,
-                HibernateRepository<FornecedorEntity> {
-    boolean existsByCnpj(String cnpj);
+        JpaRepository<FornecedorEntity, Integer>,
+        HibernateRepository<FornecedorEntity> {
+
+    boolean existsByCnpj(final String cnpj);
+
+    @EntityGraph(attributePaths = {"endereco", "endereco.cidade", "endereco.cidade.estado", "endereco.cidade.estado.pais", "contatos", "contatos.tipos"})
+    @Override
+    Optional<FornecedorEntity> findById(final Integer id);
 }
